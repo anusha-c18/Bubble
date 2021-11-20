06007e8b886c054ec2e0b72d96fde1153abab7b0
Anusha Chaturvedi <68815162+anusha-c18@users.noreply.github.com>
2021-11-20 17:20:49 +0530

    Responsive navigation bar, gallery

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
-let a=[1,2];
-a.pop;
-console.log(a.length)
+const burger = document.querySelector(".burger");
+const logo = document.querySelector(".logo");
+const navbar = document.querySelector(".n-height");
+var x = document.getElementsByClassName("nav");
+var y = document.getElementsByClassName("nv");
+
+burger.addEventListener("click", mobileMenu);
+var c=0;
+function mobileMenu() {
+    l = x.length;
+    l1 = y.length;
+    for (i = 0; i < l; i++) {
+      x[i].classList.toggle('n-visibility');
+    }
+    for (i = 0; i < l1; i++) {
+      if(y[i].classList.contains("logo1"))
+        continue;
+      y[i].style.margin="2px 0px 0px 0px";
+    }
+    logo.classList.toggle('n-visibility');
+    navbar.classList.toggle('n-height');
+    c++;
+    // document.querySelector(".nv").style.display="none";
+    if(c%2!=0)
+    {
+      document.querySelector(".logo1").style.transition="none";
+      document.querySelector(".logo1").style.opacity="0";
+    }
+    else
+    {
+      document.querySelector(".logo1").style.transition="opacity 0.5s linear";
+        document.querySelector(".logo1").style.opacity="1";
+    }
+}
