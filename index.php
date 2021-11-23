71dc0644ef7ad85fcf18d4cf65e03d86d91a7b17
Anusha Chaturvedi <68815162+anusha-c18@users.noreply.github.com>
2021-11-23 11:33:22 +0530

    update index.php

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php include_once("./html/index.html"); ?>
+<?php include_once("./html/index.html");
+include_once("./html/gallery.html");
+include_once("./html/records.html");
+include_once("./html/about.html");
+ ?>
